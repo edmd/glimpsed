cff247a9d7bab0c3b415595777a5bc4a26924188
Eryn Mac Donald <eryn.macdonald@gmail.com>
2025-03-20 13:29:06 +0000

    template

---
+++ start-all.bat
@@ -0,0 +1,2 @@
+start cmd /k "cd glimpsed-api && npm run start"
+start cmd /k "cd glimpsed-ui && ng serve"
---
+++ start-all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ../glimpsed-api && npm run start & 
+cd ../glimpsed-ui && ng serve
